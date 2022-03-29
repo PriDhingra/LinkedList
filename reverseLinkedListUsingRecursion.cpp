0d2faa4f12ee56c9e118697ba1b608e7f5e76355
Prince Dhingra <princedhingra25@gmail.com>
2022-03-29 09:59:03 -0400

    Create reverseLinkedListUsingRecursion.cpp

---
+++ reverseLinkedListUsingRecursion.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node* next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    };
+
+};
+
+void insertAtBeginning(Node* &head, int data) {
+    Node* newNode = new Node(data);
+    newNode -> next = head;
+    head = newNode;
+}
+
+void insertAtEnd(Node* &head, int data) {
+    Node* newNode = new Node(data);
+    Node* temp = head;
+
+    while(temp -> next != NULL) 
+        temp = temp -> next;
+
+    temp -> next = newNode;
+    
+}
+
+void insertAtPosition(Node* &head, int data, int pos) {
+    Node* newNode = new Node(data);
+    Node* temp = head;
+
+    if(pos == 1) {
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    while(pos - 1 > 1) {
+        temp = temp -> next;
+        pos--;
+    }
+
+    if(temp -> next == NULL) {
+        insertAtEnd(head, data);
+        return;
+    }
+
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+}
+
+void reverseLinkedList(Node* &head, Node* &curr, Node* &prev, Node* &forward) {
+    if(curr == NULL) {
+        head = prev;
+        return;
+    }
+
+    
+    forward = curr -> next;
+    curr -> next = prev;
+    prev = curr;
+    curr = forward;
+    
+    reverseLinkedList(head, curr, prev, forward);
+}
+
+void printLinkedList(Node* &head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout<<temp -> data<<" ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    Node* head = NULL;
+
+    insertAtBeginning(head, 2);
+    insertAtBeginning(head, 4);
+    insertAtBeginning(head, 6);
+    insertAtBeginning(head, 8);
+    insertAtBeginning(head, 10);
+    insertAtEnd(head, 12);
+    insertAtEnd(head, 14);
+    insertAtEnd(head, 16);
+    insertAtPosition(head, 18, 1);
+    insertAtPosition(head, 20, 2);
+    insertAtPosition(head, 24, 4);
+    insertAtPosition(head, 24, 12);
+
+    printLinkedList(head);
+
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* forward = NULL;
+    reverseLinkedList(head, curr, prev, forward);
+
+    printLinkedList(head);
+
+    return 0;
+}
