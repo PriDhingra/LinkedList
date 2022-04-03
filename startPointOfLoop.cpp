fb906a89d72bc69ba0617fcae8eb9fbf0ebde1e6
Prince Dhingra <princedhingra25@gmail.com>
2022-04-02 23:53:09 -0400

    Create startPointOfLoop.cpp

---
+++ startPointOfLoop.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node* next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    };
+
+};
+
+void insertAtBeginning(Node* &head, int data) {
+    Node* newNode = new Node(data);
+    newNode -> next = head;
+    head = newNode;
+}
+
+void insertAtEnd(Node* &head, int data) {
+    Node* newNode = new Node(data);
+    Node* temp = head;
+
+    while(temp -> next != NULL) 
+        temp = temp -> next;
+
+    temp -> next = newNode;
+    
+}
+
+void insertAtPosition(Node* &head, int data, int pos) {
+    Node* newNode = new Node(data);
+    Node* temp = head;
+
+    if(pos == 1) {
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    while(pos - 1 > 1) {
+        temp = temp -> next;
+        pos--;
+    }
+
+    if(temp -> next == NULL) {
+        insertAtEnd(head, data);
+        return;
+    }
+
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+}
+
+
+Node* startNodeOfLoop(Node* &head) {
+    if(head == NULL) 
+        return NULL;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    bool temp = false;;
+
+    while(slow != NULL && fast != NULL && !temp) {
+        fast = fast -> next;
+        if(fast != NULL)
+            fast = fast -> next;
+
+        slow = slow -> next;
+
+        if(slow == fast) {
+            temp = true;
+            slow = head;
+        }
+    }
+
+    while(slow != fast) { 
+        slow = slow -> next;
+        fast = fast -> next;
+    }
+
+    return slow;
+}
+
+void insertLoopInLinkedList(Node* &head) {
+    Node* temp = head;
+
+    while(temp -> next != NULL) 
+        temp = temp -> next;
+
+    temp -> next = head -> next -> next;
+}
+
+int main() {
+    Node* head = NULL;
+
+    insertAtBeginning(head, 2);
+    insertAtBeginning(head, 4);
+    insertAtBeginning(head, 6);
+    insertAtBeginning(head, 8);
+    insertAtBeginning(head, 10);
+    insertAtEnd(head, 12);
+    insertAtEnd(head, 14);
+    insertAtEnd(head, 16);
+    insertAtPosition(head, 90, 1);
+    insertAtPosition(head, 20, 2);
+    insertAtPosition(head, 24, 4);
+    insertAtPosition(head, 26, 4);
+    insertAtPosition(head, 18, 1);
+    insertAtPosition(head, 20, 2);
+    insertAtPosition(head, 24, 4);
+    insertAtPosition(head, 26, 4);
+
+    insertLoopInLinkedList(head);
+
+    Node* startPoint = startNodeOfLoop(head);
+
+    cout<<"Start Point of Loop is "<<startPoint -> data;
+
+    return 0;
+}
